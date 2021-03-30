5b4a9c15112867cc26a53381ad10b7c29f5786cf
FloorCheese <ryan.der23@ycdsbk12.ca>
2021-03-30 16:30:16 +0000

    added random code background doesn't work

--- main.py
+++ main.py
@@ -1,67 +1,58 @@
-""" 
-A basic pygame template
-"""
- 
 import pygame
- 
-# Define some colors
-BLACK = (   0,   0,   0)
-WHITE = ( 255, 255, 255)
-GREEN = (   0, 255,   0)
-RED = ( 255,   0,   0)
- 
+
 pygame.init()
-  
-# Set the width and height of the screen [width, height]
-size = (701, 501)
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GREY = (220, 220, 220)
+
+size = (800, 600)
 screen = pygame.display.set_mode(size)
- 
-pygame.display.set_caption("My Game")
- 
-#Loop until the user clicks the close button.
-done = False
- 
-# Used to manage how fast the screen updates
+
+pygame.display.set_caption("Virus Fighter")
+
+run = True
 clock = pygame.time.Clock()
- 
-#box properties
-box_x = 50
-box_y = 50
-box_width = 3
-box_height = 3
 
-# -------- Main Program Loop -----------
-def main():
-  run = True
-  while run:
-    for event in pygame.event.get(): #Check for user actions
-        if event.type == pygame.QUIT: 
-            run = False #End loop
+box_1x = 50  #all numbers are placeholders
+box_1y = 200
+box_2x = 200
+box_2y = 200
+box_width = 100
+box_height = 50
 
-  
-    # --- Game logic should go here
- 
-    # --- Drawing code should go here
-     
-    # First, clear the screen to white or whatever background colour. 
-    # Don't put other drawing commands above this, or they will be erased with this command.
-    screen.fill(WHITE)
-    font = pygame.font.Font('freesansbold.ttf',36)
+button_startgame = False
+button_help = False
+button_back = False
+mouse_click_position = [0,0]
 
-title_x = 351
-title_y = 400
-def title_screen(x, y):
-  title = font.render("Stop The Virus",True, (WHITE)) #working title
-  screen.blit(title, (title_x, title_y))
+while run:
 
-pygame.draw.rect(screen, RED, [box_x, box_y, box_width, box_height])
-    # --- Go ahead and update the screen with what we've drawn.
-pygame.display.flip()
- 
-    # --- Limit to 60 frames per second
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+        run = False
+    elif event.type == pygame.MOUSEBUTTONDOWN:
+      mouse_click_position = pygame.mouse.get_pos()
+
+screen.fill(WHITE)
+pygame.display.update()
+
+font = pygame.font.SysFont('Calibri', 25, True, False)
+title = font.render("Virus Fighter", True, BLACK)
+screen.blit(title, [500, 200])
+
+pygame.display.update()
+
+
+
+pygame.draw.rect(screen, GREY, [box_1x, box_1y, box_width, box_height])
+
+pygame.draw.rect(screen, GREY, [box_2x, box_2y, box_width, box_height])
+
+
 clock.tick(60)
-     
-# Close the window and quit.
-# If you forget this line, the program will 'hang'
-# on exit if running from IDLE.
+
 pygame.quit()
