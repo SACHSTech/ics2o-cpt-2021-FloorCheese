6960de6317866de4485f896a93bc129b66273b9b
FloorCheese <>
2021-04-08 21:47:42 +0000

    sides have collision

--- main.py
+++ main.py
@@ -25,6 +25,10 @@ bounce_2_x = 520
 bounce_2_y = 0
 movespeed = 0.2
 
+block_1 = pygame.Rect(bounce_width, bounce_height, bounce_1_x, bounce_1_y)
+block_2 = pygame.Rect(bounce_width, bounce_height, bounce_2_x, bounce_2_y)
+full_box = pygame.Rect(box_x, box_y, box_width, box_height)
+
 player_1_score = 0
 player_2_score = 0
 clock = pygame.time.Clock()
@@ -46,10 +50,10 @@ while run:
   if keys[pygame.K_DOWN] and bounce_2_y < screen_height - bounce_height - movespeed:
     bounce_2_y += movespeed
 
-  if box_x == bounce_1_x:
+  if (box_x < bounce_1_x + bounce_width and box_x + box_width > bounce_1_x and box_y < bounce_1_y + bounce_height and box_y + box_y > bounce_1_y):
     box_speed_x = box_speed_x * -1
 
-  if box_x == bounce_2_x:
+  if (box_x < bounce_2_x + bounce_width and box_x + box_width > bounce_2_x and box_y < bounce_2_y + bounce_height and box_y + box_height > bounce_2_y):
     box_speed_x = box_speed_x * -1
 
   if box_y + box_height >= screen_height or box_y <= 0:
